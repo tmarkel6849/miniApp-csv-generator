c9b76832f66f95011cd3a94ab2ed8d141b546080
Trevor Markel <tmarkel6849@gmail.com>
2019-05-07 11:47:08 -0600

    add post route, body-parser middleware

--- public/app.js
+++ public/app.js
@@ -5,4 +5,4 @@ document.getElementById('form').addEventListener('submit', e => {
   let formValue = form.children[1].value
   console.log(JSON.stringify(formValue));
   form.children[1].value = "";
-})
+});
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const bodyPareser = require('body-parser');
 
 const app = express();
 const port = 3000;
-
+/*___________________INITIALIZE______________________*/
 app.listen(port, (err, result) => {
   if (err) {
     console.log('This is the error', err);
@@ -12,10 +13,19 @@ app.listen(port, (err, result) => {
   console.log('CSV Generator Starting Up...');
 });
 
+/*____________________MIDDLEWARE_____________________*/
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
+/*_____________________ROUTES________________________*/
 app.get('/', (req, res) => {
-  res.render('index');
+  res.sendStatus(200);
+});
+
+app.post('/csv', (req, res) => {
+  let contents = req.body;
+  console.log(contents)
+  res.send('I came from the server!!');
 });
 
 
