ed4041500f4efd4bc5d6a8ac6c9dc5aec83bb94c
Trevor Markel <tmarkel6849@gmail.com>
2019-05-07 18:53:49 -0600

    parsing into csv working on server

--- index.js
+++ index.js
@@ -1,9 +1,51 @@
-const app = require('./server.js');
-const port = 3000;
 
-app.listen(port, (err, result) => {
-  if (err) {
-    console.log('This is the error', err);
+
+let csvText = ``;
+
+const newLine = () => {
+  return `
+`;
+}
+
+const makeCSV = (data) => {
+  for (let key in data) {
+    if (key === 'children') break;
+    csvText += `${key},`;
   }
-  console.log('CSV Generator Starting Up...');
-});
+  csvText = csvText.slice(0, csvText.length - 1);
+  csvText += newLine();
+  for (let key in data) {
+    if (Array.isArray(data[key])) {
+      if (data[key].length > 0) {
+        childObj(data[key]);
+        continue;
+      } else {
+        continue;
+      }
+    }
+    csvText += `${data[key]},`;
+  }
+  csvText = csvText.slice(0, csvText.length - 1);
+}
+const childObj = (array) => {
+  for (let obj of array) {
+    csvText = csvText.slice(0, csvText.length - 1);
+    csvText += newLine();
+    for (let key in obj) {
+      if (Array.isArray(obj[key])) {
+        if (obj[key].length > 0) {
+          csvText = csvText.slice(0, csvText.length - 1);
+          csvText += newLine();
+          childObj(obj[key]);
+          continue;
+        } else {
+          continue;
+        }
+      }
+      csvText += `${obj[key]},`;
+    }
+  }
+}
+module.exports.makeCSV = makeCSV;
+module.exports.childObj = childObj;
+module.exports.newLine = newLine;
--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const bodyParser = require('body-parser');
+// const makeCSV = require('./index.js').makeCSV;
+// const chilObj = require('./index.js').childObj;
+// const newLine = require('./index.js').newLine;
+
 
 const app = express();
 const port = 3000;
@@ -23,14 +27,61 @@ app.get('/', (req, res) => {
 });
 
 app.post('/csv', (req, res) => {
-  let clientData = req.body;
-  let dataArr = clientData.split('=');
-  console.log(dataArr[1].trim());
+  let sentData = req.body;
+  sentData = sentData.split('=');
+  let data = sentData[1].trim();
+  let parsedData = JSON.parse(data);
+  makeCSV(parsedData)
+  console.log(csvText);
  res.redirect('/');
 });
 
+let csvText = ``;
 
+const newLine = () => {
+  return `
+`;
+}
 
+const makeCSV = (data) => {
+  for (let key in data) {
+    if (key === 'children') break;
+    csvText += `${key},`;
+  }
+  csvText = csvText.slice(0, csvText.length - 1);
+  csvText += newLine();
+  for (let key in data) {
+    if (Array.isArray(data[key])) {
+      if (data[key].length > 0) {
+        childObj(data[key]);
+        continue;
+      } else {
+        continue;
+      }
+    }
+    csvText += `${data[key]},`;
+  }
+  csvText = csvText.slice(0, csvText.length - 1);
+}
+const childObj = (array) => {
+  for (let obj of array) {
+    csvText = csvText.slice(0, csvText.length - 1);
+    csvText += newLine();
+    for (let key in obj) {
+      if (Array.isArray(obj[key])) {
+        if (obj[key].length > 0) {
+          csvText = csvText.slice(0, csvText.length - 1);
+          csvText += newLine();
+          childObj(obj[key]);
+          continue;
+        } else {
+          continue;
+        }
+      }
+      csvText += `${obj[key]},`;
+    }
+  }
+}
 
 
 module.exports = app;
